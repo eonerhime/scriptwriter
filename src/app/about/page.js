62531318ee2b8de83dca54b2c6ff8f7ff4769f08
Emo Onerhime <emo.onerhime@gmail.com>
2025-03-12 17:38:12 +0100

    Changes made to /page.js and about/page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-import profileImage from "@/assets/ifeoma.png";
->>>>>>> db2ca0e35e62cc6363a18fc1deeb6b78b500e5a4
 import AboutContent from "@/components/AboutContent";
 
 // Shows on About only
