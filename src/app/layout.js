57e09dd87294ccfaf64a96f1b08f00cf9580ca95
Emo Onerhime <emo.onerhime@gmail.com>
2025-04-19 23:20:42 +0100

    Add suppressHydrationWarning attribute to html tag in RootLayout for improved hydration handling

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${montserrat.className} antialiased min-w-full min-h-screen flex flex-col relative box-border`}
       >
